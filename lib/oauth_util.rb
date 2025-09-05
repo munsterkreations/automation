719c11985dc9b5e8be3ea86983df05ff363bd084
munsterkreations <104373517+munsterkreations@users.noreply.github.com>
2025-09-04 21:15:23 -0500

    Update oauth_util.rb

--- lib/oauth_util.rb
+++ lib/oauth_util.rb
@@ -1,89 +1,75 @@
-require 'google/api_client'
-require 'google/api_client/client_secrets'
-require 'json'
-require 'launchy'
-require 'thin'
+require "google/apis/youtube_v3"
+require "signet/oauth_2/client"
+require "json"
+require "launchy"
+require "webrick"
 
-RESPONSE_HTML = <<stop
+RESPONSE_HTML = <<~HTML
 <html>
-  <head>
-    <title>OAuth 2 Flow Complete</title>
-  </head>
+  <head><title>OAuth 2 Flow Complete</title></head>
   <body>
-    You have successfully completed the OAuth 2 flow. Please close this browser window and return to your program.
+    ✅ You have successfully completed the OAuth 2 flow.<br>
+    You may now close this window.
   </body>
 </html>
-stop
+HTML
 
-FILE_POSTFIX = '-oauth2.json'
+CREDENTIALS_FILE = "credentials.json"  # downloaded from Google Cloud Console
+TOKEN_FILE = "token.json"              # local storage for your tokens
 
-# Small helper for the sample apps for performing OAuth 2.0 flows from the command
-# line. Starts an embedded server to handle redirects.
 class CommandLineOAuthHelper
-
   def initialize(scope)
-    credentials = Google::APIClient::ClientSecrets.load
+    credentials = JSON.parse(File.read(CREDENTIALS_FILE))
     @authorization = Signet::OAuth2::Client.new(
-      :authorization_uri => credentials.authorization_uri,
-      :token_credential_uri => credentials.token_credential_uri,
-      :client_id => credentials.client_id,
-      :client_secret => credentials.client_secret,
-      :redirect_uri => credentials.redirect_uris.first,
-      :scope => scope
+      client_id: credentials["installed"]["client_id"],
+      client_secret: credentials["installed"]["client_secret"],
+      authorization_uri: "https://accounts.google.com/o/oauth2/auth",
+      token_credential_uri: "https://oauth2.googleapis.com/token",
+      redirect_uri: credentials["installed"]["redirect_uris"].first,
+      scope: scope
     )
   end
 
-  # Request authorization. Checks to see if a local file with credentials is present, and uses that.
-  # Otherwise, opens a browser and waits for response, then saves the credentials locally.
   def authorize
-    credentialsFile = $0 + FILE_POSTFIX
-
-    if File.exist? credentialsFile
-      File.open(credentialsFile, 'r') do |file|
-        credentials = JSON.load(file)
-        @authorization.access_token = credentials['access_token']
-        @authorization.client_id = credentials['client_id']
-        @authorization.client_secret = credentials['client_secret']
-        @authorization.refresh_token = credentials['refresh_token']
-        @authorization.expires_in = (Time.parse(credentials['token_expiry']) - Time.now).ceil
-        if @authorization.expired?
-          @authorization.fetch_access_token!
-          save(credentialsFile)
-        end
+    if File.exist?(TOKEN_FILE)
+      token_data = JSON.parse(File.read(TOKEN_FILE))
+      @authorization.update_token!(token_data)
+      if @authorization.expired?
+        @authorization.fetch_access_token!
+        save_token
       end
     else
-      auth = @authorization
-      url = @authorization.authorization_uri().to_s
-      server = Thin::Server.new('0.0.0.0', 8080) do
-        run lambda { |env|
-          # Exchange the auth code & quit
-          req = Rack::Request.new(env)
-          auth.code = req['code']
-          auth.fetch_access_token!
-          server.stop()
-          [200, {'Content-Type' => 'text/html'}, RESPONSE_HTML]
-        }
-      end
-
-      Launchy.open(url)
-      server.start()
-
-      save(credentialsFile)
+      start_local_server
     end
 
-    return @authorization
+    @authorization
   end
 
-  def save(credentialsFile)
-    File.open(credentialsFile, 'w', 0600) do |file|
-      json = JSON.dump({
-        :access_token => @authorization.access_token,
-        :client_id => @authorization.client_id,
-        :client_secret => @authorization.client_secret,
-        :refresh_token => @authorization.refresh_token,
-        :token_expiry => @authorization.expires_at
-      })
-      file.write(json)
+  private
+
+  def start_local_server
+    server = WEBrick::HTTPServer.new(
+      Port: 8080,
+      Logger: WEBrick::Log.new(File::NULL),
+      AccessLog: []
+    )
+
+    server.mount_proc "/" do |req, res|
+      @authorization.code = req.query["code"]
+      @authorization.fetch_access_token!
+      save_token
+      res.body = RESPONSE_HTML
+      server.shutdown
+    end
+
+    Launchy.open(@authorization.authorization_uri.to_s)
+    trap("INT") { server.shutdown }
+    server.start
+  end
+
+  def save_token
+    File.open(TOKEN_FILE, "w", 0600) do |file|
+      file.write(JSON.pretty_generate(@authorization.to_h))
     end
   end
 end
